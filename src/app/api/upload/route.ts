503ef9fa802e9f7e43e998f17b2336d3fdd3cb00
sao-coding <jeter.nice@gmail.com>
2025-02-11 00:22:35 +0800

    ✨ 增加錯誤處理以改善檔案上傳 API 的穩定性

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -6,10 +6,14 @@ export const POST = async (req: NextRequest) => {
   const formData = await req.formData()
   const file = formData.get("file")
   const rootPath = "./src/data"
-  if (file) {
-    const filePath = `${rootPath}/${(file as File).name.replace(/[^a-zA-Z0-9.]/g, "").replace(/\.+/g, ".")}`
-    const fileContent = new Uint8Array(await (file as Blob).arrayBuffer())
-    await fs.writeFile(filePath, fileContent)
-    return NextResponse.json({ message: "Upload success" })
+  try {
+    if (file) {
+      const filePath = `${rootPath}/${(file as File).name.replace(/[^a-zA-Z0-9.]/g, "").replace(/\.+/g, ".")}`
+      const fileContent = new Uint8Array(await (file as Blob).arrayBuffer())
+      await fs.writeFile(filePath, fileContent)
+      return NextResponse.json({ message: `檔案 ${filePath} 上傳成功` })
+    }
+  } catch (error) {
+    return NextResponse.json({ message: "上傳失敗" }, { status: 500 })
   }
 }
